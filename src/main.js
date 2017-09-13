fb1e0dfc336aa0ca6577f12e8a832d96f841efc3
Amit Kanwar <kanwaramit92@gmail.com>
2017-09-13 11:35:20 -0400

    Adding Server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyEditor from './MyEditor';
+
+ReactDOM.render(
+    <MyEditor />,
+    document.getElementById('mount')
+);
